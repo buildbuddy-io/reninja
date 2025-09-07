0c74d9e5b1779483ba9555ff96ba7f3d1890a215
Tyler Williams <williams.tyler@gmail.com>
2025-09-06 21:51:10 -0700

    Add a state test

---
+++ internal/state/state_test.go
@@ -0,0 +1,33 @@
+package state_test
+
+import (
+	"testing"
+
+	"github.com/buildbuddy-io/gin/internal/eval_env"
+	"github.com/buildbuddy-io/gin/internal/state"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBasic(t *testing.T) {
+	s := state.New()
+
+	command := &eval_env.EvalString{}
+	command.AddText("cat ")
+	command.AddSpecial("in")
+	command.AddText(" > ")
+	command.AddSpecial("out")
+
+	rule := eval_env.NewRule("cat")
+	rule.AddBinding("command", command)
+	s.Bindings().AddRule(rule)
+
+	edge := s.AddEdge(rule)
+	s.AddIn("in1", edge)
+	s.AddIn("in2", edge)
+	s.AddOut("out", edge)
+
+	assert.Equal(t, "cat in1 in2 > out", edge.EvaluateCommand(false))
+	assert.False(t, s.GetNode("in1").Dirty())
+	assert.False(t, s.GetNode("in2").Dirty())
+	assert.False(t, s.GetNode("out").Dirty())
+}
