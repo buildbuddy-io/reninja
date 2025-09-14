227e4fccafe8c1b3c3158f5643b62feacc352713
Tyler Williams <williams.tyler@gmail.com>
2025-09-13 22:30:22 -0700

    Add implicit_dep_loader

---
+++ internal/implicit_dep_loader/implicit_dep_loader.go
@@ -0,0 +1,138 @@
+package implicit_dep_loader
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	
+	"github.com/buildbuddy-io/gin/internal/depfile_parser"
+	"github.com/buildbuddy-io/gin/internal/deps_log"
+	"github.com/buildbuddy-io/gin/internal/disk"
+	"github.com/buildbuddy-io/gin/internal/explanations"
+	"github.com/buildbuddy-io/gin/internal/graph"
+	"github.com/buildbuddy-io/gin/internal/state"
+)
+
+type ImplicitDepLoader struct {
+	state                *state.State
+	depsLog              *deps_log.DepsLog
+	diskInterface        disk.Interface
+	depfileParserOptions depfile_parser.DepfileParserOptions
+	explanations         *explanations.OptionalExplanations
+}
+
+func New(state *state.State, depsLog *deps_log.DepsLog, diskInterface disk.Interface, depfileParserOptions depfile_parser.DepfileParserOptions, explanations *explanations.OptionalExplanations) *ImplicitDepLoader {
+	return &ImplicitDepLoader{
+		state: state,
+		depsLog: depsLog,
+		diskInterface: diskInterface,
+		depfileParserOptions: depfileParserOptions,
+		explanations: explanations,
+	}
+}
+
+// LoadDeps returns a boolean indicating if load succeeded and an error.
+// If error is set, it's a failure, if loading failed, it's up to the
+// caller to decide what to do.
+func (l *ImplicitDepLoader) LoadDeps(edge *graph.Edge) (bool, error) {
+	depsType := edge.GetBinding("deps")
+	if depsType != "" {
+		return l.LoadDepsFromLog(edge)
+	}
+	depfile := edge.GetUnescapedDepfile()
+	if depfile != "" {
+		return l.LoadDepFile(edge, depfile)
+	}
+	// No deps to load.
+	return true, nil
+}
+
+// Returns loaded, error
+func (l *ImplicitDepLoader) LoadDepFile(edge *graph.Edge, path string) (bool, error) {
+	content, err := l.diskInterface.ReadFile(path)
+	if err != nil {
+		// file not found is fine, ignore it.
+		if errors.Is(err, fs.ErrNotExist) {
+			err = nil
+		} else {
+			return false, err
+		}
+	}
+	firstOutput := edge.Outputs()[0]
+	if len(content) == 0 {
+		l.explanations.Record(firstOutput, "depfile '%s' is missing", path)
+		return false, nil
+	}
+
+	depfile := depfile_parser.New(l.depfileParserOptions)
+	if err := depfile.Parse(string(content)); err != nil {
+		return false, fmt.Errorf("%s: %s", path, err)
+	}
+	primaryOut := depfile.Outs()[0]
+	primaryOut, _ = graph.CanonicalizePath(primaryOut)
+
+	// Check that this depfile matches the edge's output, if not return false to
+	// mark the edge as dirty.
+	opath := firstOutput.Path()
+	if opath != primaryOut {
+		l.explanations.Record(firstOutput, "expected depfile '%s' to mention '%s', got '%s'",
+			path, firstOutput.Path(), primaryOut)
+		return false, nil
+	}
+
+	// Ensure that all mentioned outputs are outputs of the edge.
+	for _, o := range depfile.Outs() {
+		found := false
+		for _, edgeOut := range edge.Outputs() {
+			if o == edgeOut.Path() {
+				found = true
+			}
+			break
+		}
+		if !found {
+			return false, fmt.Errorf("%s: depfile mentions '%s' as an output, but no such output was declared", path, o)
+		}
+	}
+	return true, l.ProcessDepfileDeps(edge, depfile.Ins())	
+}
+
+func (l *ImplicitDepLoader) ProcessDepfileDeps(edge *graph.Edge, ins []string) error {
+	// TODO(tylerw): preallocate space in edge.inputs
+	for _, in := range ins {
+		in, _ = graph.CanonicalizePath(in)
+		node := l.state.GetNode(in)
+		edge.AddInput(node)
+		node.AddOutEdge(edge)
+	}
+	return nil
+}
+
+func (l *ImplicitDepLoader) LoadDepsFromLog(edge *graph.Edge) (bool, error) {
+	// NOTE: deps are only supported for single-target edges.
+	output := edge.Outputs()[0]
+	var deps *deps_log.Deps
+	if l.depsLog != nil {
+		deps = l.depsLog.GetDeps(output)
+	}
+	if deps == nil {
+		l.explanations.Record(output, "deps for '%s' are missing", output.Path())
+		return false, nil
+	}
+
+	// Deps are invalid if the output is newer than the deps.
+	if (output.Mtime() > deps.Mtime) {
+		l.explanations.Record(output, "stored deps info out of date for '%s' (%d vs %d)", output.Path(), deps.Mtime, output.Mtime())
+		return false, nil
+	}
+
+	nodes := deps.Nodes
+	nodeCount := len(deps.Nodes)
+	for _, node := range nodes {
+		edge.AddInput(node)
+	}
+	edge.SetImplicitDeps(edge.GetImplicitDeps() + nodeCount)
+	for _, node := range nodes {
+		node.AddOutEdge(edge)
+	}
+	return true, nil
+}
