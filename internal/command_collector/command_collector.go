f1bc8ef4b4b516e4cb1028245dba07c91cbd46bc
Tyler Williams <williams.tyler@gmail.com>
2025-09-18 15:58:07 -0700

    Add a command collector and tests

---
+++ internal/command_collector/command_collector.go
@@ -0,0 +1,58 @@
+package command_collector
+
+import (
+	"fmt"
+	"github.com/buildbuddy-io/gin/internal/graph"
+)
+
+type CommandCollector struct {
+	visitedNodes map[*graph.Node]struct{}
+	visitedEdges map[*graph.Edge]struct{}
+	inEdges []*graph.Edge
+}
+
+func New() *CommandCollector {
+	return &CommandCollector{
+		visitedNodes: make(map[*graph.Node]struct{}, 0),
+		visitedEdges: make(map[*graph.Edge]struct{}, 0),
+		inEdges: make([]*graph.Edge, 0),
+	}
+}
+func (c *CommandCollector) InEdges() []*graph.Edge {
+	return c.inEdges
+}
+
+func (c *CommandCollector) CollectFrom(node *graph.Node) {
+	if node == nil {
+		panic("node should not be nil")
+	}
+	if _, ok := c.visitedNodes[node]; ok {
+		fmt.Printf("already seen %s\n", node.Path())
+		return
+	}
+	c.visitedNodes[node] = struct{}{}
+	fmt.Printf("first time %s\n", node.Path())
+
+	edge := node.InEdge()
+	if edge == nil {
+		return
+	}
+
+	if _, ok := c.visitedEdges[edge]; ok {
+		return
+	}
+	c.visitedEdges[edge] = struct{}{}
+
+	for _, inputNode := range edge.Inputs() {
+		fmt.Printf(" Recursing on %s\n", inputNode.Path())
+	}
+	for _, inputNode := range edge.Inputs() {
+		c.CollectFrom(inputNode)
+	}
+
+	if !edge.IsPhony() {
+		c.inEdges = append(c.inEdges, edge)
+	}
+}
+
+	
---
+++ internal/command_collector/command_collector_test.go
@@ -0,0 +1,65 @@
+package command_collector_test
+
+import (
+	"testing"
+
+	"github.com/buildbuddy-io/gin/internal/command_collector"
+	"github.com/buildbuddy-io/gin/internal/state"
+	"github.com/buildbuddy-io/gin/internal/test"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCommandCollector(t *testing.T) {
+	s := state.New()
+	test.AddCatRule(t, s)
+	test.AssertParse(t, `
+build out1: cat in1
+build mid1: cat in1
+build out2: cat mid1
+build out3 out4: cat mid1
+build all: phony out1 out2 out3
+`, s)
+	{
+		collector := command_collector.New()
+		
+		// Start visit from out2; this should add `build mid1` and `build out2` to
+		// the edge list.
+		collector.CollectFrom(s.GetNode("out2"))
+		require.Equal(t, 2, len(collector.InEdges()))
+		edges := collector.InEdges()
+		edges[0].Dump("edge-0")
+		assert.Equal(t, "cat in1 > mid1", edges[0].EvaluateCommand(false))
+		assert.Equal(t, "cat mid1 > out2", edges[1].EvaluateCommand(false))
+
+		// Add a visit from out1, this should append `build out1`
+		collector.CollectFrom(s.GetNode("out1"))
+		require.Equal(t, 3, len(collector.InEdges()))
+		edges = collector.InEdges()
+		assert.Equal(t, "cat in1 > out1", edges[2].EvaluateCommand(false))
+
+		// Another visit from all; this should add edges for out1, out2 and out3,
+		// but not all (because it's phony).
+		collector.CollectFrom(s.GetNode("all"))
+		require.Equal(t, 4, len(collector.InEdges()))
+		edges = collector.InEdges()
+		assert.Equal(t, "cat in1 > mid1", edges[0].EvaluateCommand(false))
+		assert.Equal(t, "cat mid1 > out2", edges[1].EvaluateCommand(false))
+		assert.Equal(t, "cat in1 > out1", edges[2].EvaluateCommand(false))
+		assert.Equal(t, "cat mid1 > out3 out4", edges[3].EvaluateCommand(false))
+	}
+
+	{
+		collector := command_collector.New()
+
+		// Starting directly from all, will add `build out1` before `build mid1`
+		// compared to the previous example above.
+		collector.CollectFrom(s.GetNode("all"))
+		require.Equal(t, 4, len(collector.InEdges()))
+		edges := collector.InEdges()
+		assert.Equal(t, "cat in1 > out1", edges[0].EvaluateCommand(false))
+		assert.Equal(t, "cat in1 > mid1", edges[1].EvaluateCommand(false))
+		assert.Equal(t, "cat mid1 > out2", edges[2].EvaluateCommand(false))
+		assert.Equal(t, "cat mid1 > out3 out4", edges[3].EvaluateCommand(false))
+	}
+}
