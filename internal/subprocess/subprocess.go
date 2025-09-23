6a4d8916d2e24fc51fe178f09a4df4fe5f5e1f4a
Tyler Williams <williams.tyler@gmail.com>
2025-09-22 22:18:49 -0700

    Add subprocess and tests

---
+++ internal/subprocess/subprocess.go
@@ -0,0 +1,196 @@
+package subprocess
+
+import (
+	"bytes"
+	"context"
+	"os"
+	"os/exec"
+	"os/signal"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"syscall"
+
+	"github.com/buildbuddy-io/gin/internal/exit_status"
+)
+
+var (
+	once sync.Once
+	interrupted atomic.Bool
+)
+
+func handleSignals() {
+	once.Do(func() {
+		signalChannel := make(chan os.Signal, 1)
+		signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
+		go func() {
+			<-signalChannel
+			interrupted.Store(true)		
+		}()
+	})
+}
+
+type Subprocess struct {
+	ctx context.Context
+	cancelFunc context.CancelFunc
+
+	cmd *exec.Cmd
+	stdOutAndErr *bytes.Buffer
+
+	mu        *sync.Mutex
+	exitError *exec.ExitError
+	done      chan struct{}
+}
+
+func NewSubprocess(command string, useConsole bool) (*Subprocess, error) {
+	handleSignals()
+	
+	ctx, cancelFunc := context.WithCancel(context.TODO())
+	s := &Subprocess{
+		ctx: ctx,
+		cancelFunc: cancelFunc,
+		cmd: exec.CommandContext(ctx, "/bin/sh", "-c", command),
+		done: make(chan struct{}),
+		mu: &sync.Mutex{},
+	}
+	
+	if useConsole {
+		s.cmd.Stdout = os.Stdout
+		s.cmd.Stderr = os.Stdout
+	} else {
+		s.stdOutAndErr = &bytes.Buffer{}
+		s.cmd.Stdout = s.stdOutAndErr
+		s.cmd.Stderr = s.stdOutAndErr
+	}
+
+	if err := s.cmd.Start(); err != nil {
+		return nil, err
+	}
+	go func() {
+		if err := s.cmd.Wait(); err != nil {
+			s.mu.Lock()
+			s.exitError = err.(*exec.ExitError)
+			s.mu.Unlock()
+		}
+		close(s.done)
+	}()
+	return s, nil
+}
+
+func (s *Subprocess) Kill() {
+	s.cancelFunc()
+}
+
+func (s *Subprocess) GetOutput() string {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if s.stdOutAndErr == nil {
+		return ""
+	}
+	return string(s.stdOutAndErr.Bytes())
+}
+
+func (s *Subprocess) TryFinish(wait bool) bool {
+	if wait {
+		select {
+		case <-s.done:
+			return true
+		}
+	} else {
+		select {
+		case <-s.done:
+			return true
+		default:
+			return false
+		}
+	}
+}
+
+// Returns ExitSuccess on successful process exit, ExitInterrupted if
+// the process was interrupted, ExitFailure if it otherwise failed.
+func (s *Subprocess) Finish() exit_status.ExitStatusType {
+	s.TryFinish(true/*=wait*/)
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if s.exitError == nil {
+		return exit_status.ExitSuccess
+	} else if strings.Contains(s.exitError.String(), "interrupt") {
+		return exit_status.ExitInterrupted
+	} else if strings.Contains(s.exitError.String(), "terminated") {
+		return exit_status.ExitInterrupted
+	} else if strings.Contains(s.exitError.String(), "hangup") {
+		return exit_status.ExitInterrupted
+	} else {
+		return exit_status.ExitStatusType(s.exitError.ProcessState.ExitCode())
+	}
+}
+
+func (s *Subprocess) Done() bool {
+	select {
+	case <-s.done:
+		return true
+	default:
+		return false
+	}
+}
+
+type Set struct {
+	running []*Subprocess
+	finished []*Subprocess
+}
+
+func NewSet() *Set {
+	return &Set{
+		running: make([]*Subprocess, 0),
+		finished: make([]*Subprocess, 0),
+	}
+}
+
+func (s *Set) Add(command string, useConsole bool) (*Subprocess, error) {
+	sub, err := NewSubprocess(command, useConsole)
+	if err != nil {
+		return nil, err
+	}
+	s.running = append(s.running, sub)
+	return sub, nil
+}
+
+func (s *Set) TestGetRunningSubprocesses() []*Subprocess {
+	return s.running
+}
+
+func (s *Set) TestGetFinishedSubprocesses() []*Subprocess {
+	return s.finished
+}
+
+func (s *Set) DoWork() bool {
+	nextRunning := s.running[:0]
+	for _, sub := range s.running {
+		if interrupted.Load() {
+			return true
+		}
+		if sub.Done() {
+			s.finished = append(s.finished, sub)
+		} else {
+			nextRunning = append(nextRunning, sub)
+		}
+	}
+	s.running = nextRunning
+	return interrupted.Load()
+}
+
+func (s *Set) NextFinished() *Subprocess {
+	if len(s.finished) == 0 {
+		return nil
+	}
+	sub := s.finished[0]
+	s.finished = s.finished[1:]
+	return sub
+}
+
+func (s *Set) Clear() {
+	for _, sub := range s.running {
+		sub.Kill()
+	}
+	s.running = s.running[:0]
+}
---
+++ internal/subprocess/subprocess_test.go
@@ -0,0 +1,244 @@
+package subprocess_test
+
+import (
+	"fmt"
+	"runtime"
+	"syscall"
+	"testing"
+
+	"github.com/buildbuddy-io/gin/internal/subprocess"
+	"github.com/buildbuddy-io/gin/internal/exit_status"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBadCommandStderr(t *testing.T) {
+	subprocs := subprocess.NewSet()
+	subproc, err := subprocs.Add("cmd /c ninja_no_such_command", false /*=useConsole*/)
+	require.NoError(t, err)
+	require.NotNil(t, subproc)
+
+	for !subproc.Done() {
+		subprocs.DoWork()
+	}
+	subprocs.DoWork()
+
+	assert.NotEqual(t, exit_status.ExitSuccess, subproc.Finish())
+}
+
+func TestNoSuchCommand(t *testing.T) {
+	subprocs := subprocess.NewSet()
+	subproc, err := subprocs.Add("ninja_no_such_command", false /*=useConsole*/)
+	require.NoError(t, err)
+	require.NotNil(t, subproc)
+
+	for !subproc.Done() {
+		subprocs.DoWork()
+	}
+	subprocs.DoWork()
+
+	assert.NotEqual(t, exit_status.ExitSuccess, subproc.Finish())
+}
+
+func TestInterruptChild(t *testing.T) {
+	subprocs := subprocess.NewSet()
+	subproc, err := subprocs.Add("kill -INT $$", false /*=useConsole*/)
+	require.NoError(t, err)
+	require.NotNil(t, subproc)
+
+	for !subproc.Done() {
+		subprocs.DoWork()
+	}
+	subprocs.DoWork()
+
+	assert.Equal(t, exit_status.ExitStatusType(130), subproc.Finish())	
+}
+
+func TestInterruptParent(t *testing.T) {
+	subprocs := subprocess.NewSet()
+	subproc, err := subprocs.Add("kill -INT $PPID ; sleep 1", false /*=useConsole*/)
+	require.NoError(t, err)
+	require.NotNil(t, subproc)
+
+	for !subproc.Done() {
+		interrupted := subprocs.DoWork()
+		if (interrupted) {
+			return
+		}
+	}
+
+	assert.False(t, true, "should have been interrupted")
+}
+
+func TestInterruptChildWithSigterm(t *testing.T) {
+	subprocs := subprocess.NewSet()
+	subproc, err := subprocs.Add("kill -TERM $$", false /*=useConsole*/)
+	require.NoError(t, err)
+	require.NotNil(t, subproc)
+
+	for !subproc.Done() {
+		subprocs.DoWork()
+	}
+	subprocs.DoWork()
+
+	assert.Equal(t, exit_status.ExitStatusType(130), subproc.Finish())	
+}
+
+func TestInterruptParentWithSigterm(t *testing.T) {
+	subprocs := subprocess.NewSet()
+	subproc, err := subprocs.Add("kill -TERM $PPID ; sleep 1", false /*=useConsole*/)
+	require.NoError(t, err)
+	require.NotNil(t, subproc)
+
+	for !subproc.Done() {
+		interrupted := subprocs.DoWork()
+		if (interrupted) {
+			return
+		}
+	}
+
+	assert.False(t, true, "should have been interrupted")
+}
+
+func TestInterruptChildWithSighup(t *testing.T) {
+	subprocs := subprocess.NewSet()
+	subproc, err := subprocs.Add("kill -HUP $$", false /*=useConsole*/)
+	require.NoError(t, err)
+	require.NotNil(t, subproc)
+
+	for !subproc.Done() {
+		subprocs.DoWork()
+	}
+	subprocs.DoWork()
+
+	assert.Equal(t, exit_status.ExitStatusType(130), subproc.Finish())	
+}
+
+func TestInterruptParentWithSighup(t *testing.T) {
+	subprocs := subprocess.NewSet()
+	subproc, err := subprocs.Add("kill -HUP $PPID ; sleep 1", false /*=useConsole*/)
+	require.NoError(t, err)
+	require.NotNil(t, subproc)
+
+	for !subproc.Done() {
+		interrupted := subprocs.DoWork()
+		if (interrupted) {
+			return
+		}
+	}
+
+	assert.False(t, true, "should have been interrupted")
+}
+
+func getSimpleCommand() string {
+	if runtime.GOOS == "windows" {
+		return "cmd /c dir \\"
+	} else {
+		return "ls /"
+	}
+}
+
+func TestSetWithSingle(t *testing.T) {
+        subprocs := subprocess.NewSet()
+	subproc, err := subprocs.Add(getSimpleCommand(), false /*=useConsole*/)
+        require.NoError(t, err)
+        require.NotNil(t, subproc)
+
+        for !subproc.Done() {
+                subprocs.DoWork()
+        }
+	subprocs.DoWork()
+
+        assert.Equal(t, exit_status.ExitStatusType(0), subproc.Finish())
+	assert.NotEqual(t, "", subproc.GetOutput())
+}
+
+func TestSetWithMulti(t *testing.T) {
+	subprocs := subprocess.NewSet()
+	
+	processes := make([]*subprocess.Subprocess, 3)
+	commands := make([]string, 3)
+	commands[0] = getSimpleCommand()
+	if runtime.GOOS == "windows" {
+		commands[1] = "cmd /c echo hi"
+		commands[2] = "cmd /c time /t"
+	} else {
+		commands[1] = "id -u"
+		commands[2] = "pwd"
+	}
+
+	var err error
+	for i := range 3 {
+		processes[i], err = subprocs.Add(commands[i], false /*=useConsole*/)
+		require.NoError(t, err)
+		require.NotNil(t, processes[i])
+	}
+
+	require.Equal(t, 3, len(subprocs.TestGetRunningSubprocesses()))
+
+	for i := range 3 {
+		require.False(t, processes[i].Done())
+		require.Equal(t, "", processes[i].GetOutput())
+	}
+
+	for !processes[0].Done() || !processes[1].Done() || !processes[2].Done() {
+		require.Greater(t, len(subprocs.TestGetRunningSubprocesses()), 0)
+		subprocs.DoWork()
+	}
+	subprocs.DoWork() // required b/c subproc can be Done and doWork not know it yet.
+
+	require.Equal(t, 0, len(subprocs.TestGetRunningSubprocesses()))
+	require.Equal(t, 3, len(subprocs.TestGetFinishedSubprocesses()))
+	
+	for i := range 3 {
+		assert.Equal(t, exit_status.ExitStatusType(0), processes[i].Finish())
+		assert.NotEqual(t, "", processes[i].GetOutput())
+	}
+}
+
+func TestSetWithLots(t *testing.T) {
+	maxProcs := 1025
+	subprocs := subprocess.NewSet()
+
+	var rLimit syscall.Rlimit
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit); err == nil {
+		if int(rLimit.Cur) < maxProcs {
+			fmt.Printf("Raise [ulimit -n] above %d (currently %d) to make this test go\n", maxProcs, rLimit.Cur)
+			return
+		}
+		fmt.Printf("cur: %d, maxProxs: %d\n", int(rLimit.Cur), maxProcs)
+	}
+	
+	processes := make([]*subprocess.Subprocess, 0)
+	for range maxProcs {
+		subproc, err := subprocs.Add("/bin/echo", false /*=useConsole*/)
+		require.NoError(t, err)
+		require.NotNil(t, subproc)
+		processes = append(processes, subproc)
+	}
+
+	for len(subprocs.TestGetRunningSubprocesses()) > 0 {
+		subprocs.DoWork()
+	}
+
+	for i := range maxProcs {	
+		assert.Equal(t, exit_status.ExitStatusType(0), processes[i].Finish())
+		assert.NotEqual(t, "", processes[i].GetOutput())
+	}
+	
+	require.Equal(t, maxProcs, len(subprocs.TestGetFinishedSubprocesses()))
+}
+
+func TestReadStdin(t *testing.T) {
+        subprocs := subprocess.NewSet()
+	subproc, err := subprocs.Add("cat -", false /*=useConsole*/)
+	require.NoError(t, err)
+	
+        for !subproc.Done() {
+                subprocs.DoWork()
+        }
+	subprocs.DoWork()
+
+        assert.Equal(t, exit_status.ExitStatusType(0), subproc.Finish())
+	require.Equal(t, 1, len(subprocs.TestGetFinishedSubprocesses()))
+}
