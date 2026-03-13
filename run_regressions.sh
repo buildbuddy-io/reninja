72004c0964cfbc3cdacb8eb8c83a45fa69ac3bbf
Tyler Williams <williams.tyler@gmail.com>
2026-03-12 21:51:27 -0700

    Cleanup

--- run_regressions.sh
+++ run_regressions.sh
@@ -9,6 +9,7 @@ if [ ! -f reninja ]; then
 fi
 
 touch MODULE.bazel  # trick bb remote into running here.
+trap 'rm -f MODULE.bazel' EXIT SIGINT SIGTERM
 
 UBUNTU_2404="docker://gcr.io/flame-public/rbe-ubuntu24-04@sha256:f7db0d4791247f032fdb4451b7c3ba90e567923a341cc6dc43abfc283436791a"
 BB_REMOTE_COMMON_FLAGS="--runner_exec_properties=init-dockerd=true"
@@ -23,4 +24,3 @@ for RECIPE in "${RECIPES[@]}"; do
     echo "============ BUILDING $RECIPE ============"
     bb remote --container_image=$UBUNTU_2404 $BB_REMOTE_COMMON_FLAGS --runner_exec_properties=salt=$RECIPE --script=$RECIPE
 done
-
